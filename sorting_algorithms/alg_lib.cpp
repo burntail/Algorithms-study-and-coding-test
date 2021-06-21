631c044e4d2d552ea03ffcf5bde601e62c24d8dc
Beomtae Kim <bumtae0209@naver.com>
2021-06-21 11:24:11 +0900

    <algorithms> used

---
+++ sorting_algorithms/alg_lib.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+int main()
+{
+    vector<int> v;
+
+    int n;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cin>>n;
+
+        v.push_back(n);
+    }
+
+    sort(v.begin(),v.end());
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    
+    cout<<"\n";
+
+    
+}
+
